d2b216cd088aaf639d6d0bb468fe005e27ecad32
JJ <jc@orbsdigital.com>
2021-09-05 19:03:23 +1000

    Completed assignment

---
+++ index.js
@@ -0,0 +1,52 @@
+//Lab practice
+//Define functions add(), subtract(), multiply(), divide()
+function add(){
+}
+
+function subtract(){
+}
+
+function multiply(){
+}
+
+function divide(){
+}
+
+//use of fundamental math operators +, -, *, / (a,b) to return results
+function add(a,b){
+    return a + b;
+}
+
+function subtract(a,b){
+    return a - b;
+}
+
+function multiply(a,b){
+    return a * b;
+}
+
+function divide(a,b){
+    return a / b;
+}
+
+//increment(n) increments n and returns the result
+function increment(a){
+    return a + 1;
+}
+
+//decrement(n) decrement n and returns the result
+function decrement(a){
+    return a - 1;
+}
+
+//makeInt(string) should take in a string, parse it into a base 10 integer and return it.
+function makeInt(a){
+    return parseInt(a,10);
+}
+makeInt("2");
+
+//preserveDecimal(string) should take in a string, parse it into a float and return it.
+function preserveDecimal(n){
+    return parseFloat(n);
+}
+preserveDecimal("2.222");
